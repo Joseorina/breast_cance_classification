7be976294478da8ff36dd8383154351d41c592f4
Joseorina <orinangare@gmail.com>
2019-11-17 15:31:16 +0300

    Breast Cancer

--- breast_cancer.py
+++ breast_cancer.py
@@ -42,3 +42,38 @@ y_pred = classification.predict(X_test)
 from sklearn.metrics import confusion_matrix, classification_report
 cm = confusion_matrix(y_test, y_pred)
 sns.heatmap(cm ,annot=True)
+
+# improving the model
+min_train = X_train.min()
+range_train = (X_train - min_train).max()
+X_train_scaled = (X_train - min_train)/range_train
+#unscaled x_train
+sns.scatterplot(x =X_train['mean area'], y =X_train['mean smoothness'], hue =y_train )
+
+# scaled values
+sns.scatterplot(x =X_train_scaled['mean area'], y =X_train_scaled['mean smoothness'], hue =y_train )
+
+min_test = X_test.min()
+range_test = (X_test - min_test).max()
+X_test_scaled = (X_test - min_test)/range_test
+
+classification.fit(X_train_scaled, y_train)
+
+y_pred = classification.predict(X_test_scaled)
+
+cm = confusion_matrix(y_test, y_pred)
+
+sns.heatmap(cm, annot=True)
+
+print(classification_report(y_test, y_pred))
+
+# Improcing model part 2
+param_grid = {'c':[0.1,1,10,100], 'gamma':['1, 0.1,0.01,0.001'], 'kerne':['rbf']}
+
+from sklearn.model_selection import GridSearchCV
+grid = GridSearchCV(SVC(),param_grid, refit = True, verbose = 4)
+grid.fit(X_train_scaled, y_train)
+
+grid_prediction = grid.fit(X_test_scaled)
+
+cm = confusion_matrix(y_test, grid_prediction)

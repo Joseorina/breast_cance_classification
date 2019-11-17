471d5093ad278f8e22ffaaf2b57c93d98d5615f3
Joseorina <orinangare@gmail.com>
2019-11-17 12:30:27 +0300

    Breast Cancer

--- breast_cancer.py
+++ breast_cancer.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+#importing data
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+print(cancer['DESCR'])
+print(cancer['target'])
+
+df_cancer = pd.DataFrame(np.c_[cancer['data'], cancer['target']], columns = np.append(cancer['feature_names'], ['target']))
